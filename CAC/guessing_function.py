2344fce38a6217406ec4a4b89c277bc2b37c6cab
yechanpark <yechanpark07@gmail.com>
2023-09-16 23:17:29 -0700

    added daily_functions.json and changed the function game to take its test cases and target function directly from the json data

--- CAC/guessing_function.py
+++ CAC/guessing_function.py
@@ -1,16 +1,20 @@
 import json
+import datetime
 
-# namespace is necessary for functions within the txt file to appear
-# in the global namespace while using the exec() command
-namespace = {}
+day = 0
 
-# these are the test cases (changes daily)
-input_data = [[0,1,2,3,4,5,6],[3,4,2],[123,232,1],[1234]]
+# takes in the json data into python format from daily_functions.json
+jsondata=None
+with open('daily_functions.json') as f:
+    rawjson = f.read()
+    jsondata = json.loads(rawjson)
 
-# this is the target function (changes daily)
-def target_func(x):
-    x = list(x)
-    return int(len(x))+int(sum(x))
+
+input_data = jsondata[day]['test_cases']
+
+# this stores the target_func() in the local namespace from the json file
+exec(jsondata[day]['function'],globals(),locals())
+
 
 # this reads the text file from test_code.txt
 test_code = None
@@ -19,14 +23,13 @@ with open('test_code.txt') as f:
 
 
 try: # in case there are errors in the actual code in test_code
-    exec(test_code, namespace)
-    if 'test_func' in namespace: # checks that the user defined their function as test_func
+    exec(test_code, globals(),locals()) # STORES test_func from test_code IN THE LOCAL NAMESPACE
+    if 'test_func' in locals(): # checks that the user defined their function as test_func
         for i in input_data: #compares each test case with the target_func and test_func
-            if target_func(i) == namespace['test_func'](i):
-                print(f'{i}: true {target_func(i)}')
+            if target_func(i) == test_func(i):
+                print(f'{i}: true '+str(target_func(i)))
             else:
-                x=namespace['test_func'](i)
-                print(f'{i}: false, got {x} expected {target_func(i)}')
+                print(f'{i}: false, got {test_func(i)} expected '+str(target_func(i)))
     else:
         print('test_func() not found in code')
 except Exception as e: #catches any exceptions while running test_code
@@ -37,6 +40,6 @@ while True: #separate block for the section where the user will test their own c
     if test_input == '': #press enter to escape while loop
         break
     try:
-        print(target_func(json.loads(test_input)))
+        print(target_func(json.loads(test_input))) # [int] --> str
     except Exception as e:
         print(f'An error occurred while executing the code: {str(e)}')
