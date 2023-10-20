427afec24bfe55d64fb25470649860f6963d4e34
yechanpark <yechanpark07@gmail.com>
2023-10-19 19:28:15 -0700

    deleted print statements in Guessing_function

--- CAC/CAC/Project_Dispenser.py
+++ CAC/CAC/Project_Dispenser.py
@@ -1,31 +1,12 @@
-from datetime import datetime, timedelta
+import json
+from random import randint
 
-#keep track of current project index along with date
-project_index = 0
-last_date = datetime.now()
-#two arrays to store descriptions and video IDs
-project_descriptions = []
-video_IDs = []
+jsondata = None
 
-with open('projects.txt', 'r') as file:
-    # 
-    descriptions = file.readlines()
-
-#text file is in the format description, *new line* video 
-for i in range(0, len(descriptions), 2):
-    projects = descriptions[i].strip()
-    videos = descriptions[i+1].strip()
-    project_descriptions.append(projects)
-    video_IDs.append(videos)
-
+with open('projects.json') as f:
+    rawjson = f.read()
+    jsondata = json.loads(rawjson)
 
 def next_project():
-    global project_index, last_date
-    days_elapsed = datetime.now() - last_date
-    #project changes every 3 days
-    if days_elapsed >= timedelta(days=3):
-        last_date = datetime.now()
-        #index is updated as a mod of the number of total projects so that it automatically resets once the index surpasses total number
-        project_index = (project_index + 1) % len(project_descriptions)
-        return project_descriptions[project_index], video_IDs[project_index]
-    
+    x = randint(0,len(jsondata)-1)
+    return jsondata[x]
