bbfd127abb1cc1dd805d1dfd380d9128fe29164e
chesskid111 <chesskid168@gmail.com>
2023-10-29 10:40:40 -0700

    Project Dispenser files

---
+++ CAC/CAC/Project_Dispenser_1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+
+def dispenser():
+    df = pd.read_csv('Projects_1.csv')
+    random_index = random.randint(0,len(df['Project'])-1)
+    project = df.loc[random_index]['Project']
+    project_description = df.loc[random_index]['Project Description']
+    length_of_project = len(project)
+
+    print(project)
+    print(project_description)
+dispenser()
---
+++ CAC/CAC/Project_Dispenser_2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random
+
+def dispenser():
+    df = pd.read_csv('Projects_2.csv')
+    random_index = random.randint(0,len(df['Project'])-1)
+    project = df.loc[random_index]['Project']
+    project_description = df.loc[random_index]['Project Description']
+    length_of_project = len(project)
+
+    print(project)
+    print(project_description)
