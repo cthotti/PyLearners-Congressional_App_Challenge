d69efac8e6778d8959735ffb431a8527e2399f3c
chesskid111 <chesskid168@gmail.com>
2023-10-29 20:49:32 -0700

    new projects added

--- CAC/Project_Dispenser.py
+++ CAC/Project_Dispenser.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 
-df = pd.read_csv('/Users/chetant/VSCode/CAC/Project_Dispenser_csv.csv')
+df = pd.read_csv('Project_Dispenser_csv.csv')
 random_index = random.randint(0,len(df['Project'])-1)
 project = df.loc[random_index]['Project']
 project_description = df.loc[random_index]['Project Description']
